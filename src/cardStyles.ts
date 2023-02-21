9ce3e6be137350e8b1dbe1c0b4a7abfd10400b99
iletired <111671789+iletired@users.noreply.github.com>
2023-02-21 15:40:16 -0500

    Update cardStyles.ts

--- src/cardStyles.ts
+++ src/cardStyles.ts
@@ -1,74 +1,76 @@
 import { css } from 'lit-element'
 
 export default css`
-  .sun-card {
+  .moon-card {
     --moon-card-lines: #464646;
-    --sun-card-text-color: #fff;
-    --sun-card-subtitle-color: #fff;
+    --moon-card-text-color: #fff;
+    --moon-card-subtitle-color: #fff;
 
-    color: var(--sun-card-text-color);
+    color: var(--moon-card-text-color);
     padding: 1rem;
   }
 
-  .sun-card-body {
+  .moon-card-body {
     padding-top: 0.5rem;
   }
 
-  .sun-card.sun-card-light {
-    --sun-card-lines: #ececec;
-    --sun-card-text-color: #000;
-    --sun-card-subtitle-color: #828282;
+  .moon-card.moon-card-light {
+    --moon-card-lines: #ececec;
+    --moon-card-text-color: #000;
+    --moon-card-subtitle-color: #828282;
   }
 
-  .sun-card-header {
+  .moon-card-header {
     display: flex;
     justify-content: space-between;
   }
   
-  .sun-card-footer .sun-card-footer-row {
+  .moon-card-footer .moon-card-footer-row {
     display: flex;
     justify-content: space-around;
     padding-top: 1.5rem;
   }
 
-  .sun-card-title {
+  .moon-card-title {
     font-size: 1.5rem;
     font-weight: 500;
     padding-bottom: 2rem;
     margin: 0;
   }
 
-  .sun-card-text-container {
+  .moon-card-text-container {
     display: flex;
     flex-direction: column;
     align-items: center;
   }
 
-  .sun-card-header .sun-card-text-subtitle {
+  .moon-card-header .moon-card-text-subtitle {
     font-size: 1.15rem;
     font-weight: 400;
     padding-bottom: 0.25rem;
-    color: var(--sun-card-subtitle-color);
+    color: var(--moon-card-subtitle-color);
   }
 
-  .sun-card-header .sun-card-text-time {
+  .moon-card-header .moon-card-text-time {
     font-size: 1.85rem;
     font-weight: 400;
   }
 
-  .sun-card-footer .sun-card-text-subtitle {
+  .moon-card-footer .moon-card-text-subtitle {
     font-size: 1.25rem;
     font-weight: 400;
     padding-bottom: 0.5rem;
-    color: var(--sun-card-subtitle-color);
+    color: var(--moon-card-subtitle-color);
   }
 
-  .sun-card-footer .sun-card-text-time {
+  .moon-card-footer .moon-card-text-time {
     font-size: 1.25rem;
     font-weight: 500;
   }
 
-  .sun-card-text-time-period {
+  .moon-card-text-time-period {
     font-size: 0.75rem;
   }
 `
+
+
